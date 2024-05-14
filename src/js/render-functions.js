82e8fbd5ef8e956ab018f653a3eed207cf131b9c
SofiiaPol <sophia.polyakova05@gmail.com>
2024-05-14 22:28:56 +0300

    add styles and css-loader

--- src/js/render-functions.js
+++ src/js/render-functions.js
@@ -10,6 +10,24 @@ export const renderImages = items => {
             alt="${image.tags}"
             />
         </a>
+        <div class="info">
+                        <p class="info-item">
+                            <b>Likes</b>
+                            <span>${image.likes}</span> 
+                        </p>
+                        <p class="info-item">
+                            <b>Views</b>
+                            <span>${image.views}</span>
+                        </p>
+                        <p class="info-item">
+                            <b>Comments</b>
+                            <span>${image.comments}</span>
+                        </p>
+                        <p class="info-item">
+                            <b>Downloads</b>
+                            <span>${image.downloads}</span>
+                        </p>
+                    </div>
     </li>
 
 `
